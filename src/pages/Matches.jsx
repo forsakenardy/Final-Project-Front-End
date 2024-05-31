4ae1956d2aed60da96e8e0bfa221a67e1d9baab0
Mikel Jimenez <majc1907@gmail.com>
2024-05-31 09:22:24 +0200

    matches-form-start

--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,7 +1,34 @@
-function Matches () {
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+
+function Matches({ match, /*createMatches*/ }) {
+    const [location, setLocation] = useState("");
+    const [day, setDay] = useState("");
+    const [time, setTime] = useState("");
+    const [comment, setComment] = useState("");
+    const [pairs, setPairs] = useState(false)
+
+const handleLocation = (e)=> setLocation(e.target.value);
+const handleDay = (e)=> setDay(e.target.value);
+const handleTime = (e)=> setTime(e.target.value);
+const handleComment = (e)=> setComment(e.target.value);
+const handlePairs = (e)=> setPairs(e.target.value);
+
+
+    const handleMatchSubmit =
+    e.preventDefault();
+const requestBody = {location, day, time, comment, pairs};
+
+
     return (
-        <>
-        </>
+        <div>
+<form onSubmit={handleMatchSubmit}>
+    <label>Location:</label>
+    <input type="text" name="location" value={location} onChange={handleLocation} />
+    <label></label>
+</form>
+        </div>
     )
 }
 
